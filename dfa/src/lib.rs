c617bd4567ff9e648f8d2b64b157b97953f62180
Iurii Zamiatin <notiurii@gmail.com>
2025-04-22 22:01:01 +0100

    bug fixes and example of a problem z3 struggles on

--- dfa/src/lib.rs
+++ dfa/src/lib.rs
@@ -1,14 +1,14 @@
 use std::{
-    collections::{HashMap, HashSet},
-    ops::{BitAnd, BitOr, Div, Not},
-    u8, usize,
+    collections::{BTreeSet, HashMap},
+    fmt::Write as _,
+    ops::{BitAnd, BitOr, Not, Sub},
 };
 
 use bittle::{Bits, BitsMut};
 
 type State = u16;
 
-#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+#[derive(Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct StateBitSet {
     set: Vec<usize>,
     len: State,
@@ -17,7 +17,7 @@ struct StateBitSet {
 impl StateBitSet {
     fn new(states: State) -> StateBitSet {
         StateBitSet {
-            set: vec![0; states.div_ceil(usize::BITS as u16) as usize],
+            set: vec![0; states.div_ceil(usize::BITS as State) as usize],
             len: states,
         }
     }
@@ -49,6 +49,15 @@ impl StateBitSet {
         }
         result
     }
+
+    fn normalize_vec(states: State, set: &mut [usize]) {
+        // Ensure canoncity
+        if states % (usize::BITS as State) != 0 {
+            if let Some(last) = set.last_mut() {
+                *last &= (1usize << (states % (usize::BITS as State))).wrapping_sub(1);
+            }
+        }
+    }
 }
 
 impl BitOr for &StateBitSet {
@@ -57,11 +66,17 @@ impl BitOr for &StateBitSet {
     fn bitor(self, rhs: Self) -> Self::Output {
         assert_eq!(self.len, rhs.len);
         let mut res = self.set.clone();
-        res.symmetric_difference_assign(&rhs.set);
+        res.union_assign(&rhs.set);
         StateBitSet {
             set: res,
             len: self.len,
         }
+    }
+}
+
+impl std::fmt::Debug for StateBitSet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_set().entries(self.set.iter_ones()).finish()
     }
 }
 
@@ -79,13 +94,14 @@ impl BitAnd for &StateBitSet {
     }
 }
 
-impl Div for &StateBitSet {
+impl Sub for &StateBitSet {
     type Output = StateBitSet;
 
-    fn div(self, rhs: Self) -> Self::Output {
+    fn sub(self, rhs: Self) -> Self::Output {
         assert_eq!(self.len, rhs.len);
         let mut res = self.set.clone();
         res.difference_assign(&rhs.set);
+        StateBitSet::normalize_vec(self.len, &mut res);
         StateBitSet {
             set: res,
             len: self.len,
@@ -101,14 +117,7 @@ impl Not for StateBitSet {
         for val in res.iter_mut() {
             *val = !*val;
         }
-
-        // Ensure canoncity
-        if self.len % (State::BITS as State) != 0 {
-            if let Some(last) = res.last_mut() {
-                *last &= (1 << (self.len % (State::BITS as State))) - 1;
-            }
-        }
-
+        StateBitSet::normalize_vec(self.len, &mut res);
         StateBitSet {
             set: res,
             len: self.len,
@@ -168,7 +177,7 @@ impl Renaming {
 /// State 0 is considered the starting state and is never accepting (returning 1).
 /// No state should link to state 0 - it is used as a unique starting point.
 #[derive(Clone, Debug, PartialEq, Eq)]
-struct Dfa {
+pub(crate) struct Dfa {
     // Number of inputs. u8 is enough here, as having more inputs than that would
     // make Dfa not fit in RAM.
     inputs: u8,
@@ -197,13 +206,62 @@ impl Dfa {
         1usize << self.inputs
     }
 
-    fn alphabet(&self) -> impl Iterator<Item = usize> {
+    fn alphabet(&self) -> impl DoubleEndedIterator<Item = usize> {
         0..self.alphabet_size()
+    }
+
+    fn to_dot(&self) -> String {
+        let mut result = String::new();
+
+        let _ = writeln!(&mut result, "digraph G {{");
+        let _ = writeln!(&mut result, "  start [shape=ellipse, label=start];");
+
+        for state in self.states() {
+            let shape = if self.accepting.contains(state) {
+                "doublecircle"
+            } else {
+                "circle"
+            };
+            let _ = writeln!(&mut result, "  s{state} [shape={shape}, label={state}];");
+        }
+
+        let _ = writeln!(&mut result);
+
+        let input_label =
+            |input: usize| format!("{:0width$b}", input, width = self.inputs as usize);
+
+        for (input, init_state) in self.init_states.iter().enumerate() {
+            let _ = writeln!(
+                &mut result,
+                "  start -> s{init_state} [label={}];",
+                input_label(input)
+            );
+        }
+
+        let _ = writeln!(&mut result);
+
+        for state in self.states() {
+            for input in self.alphabet() {
+                let label = input_label(input);
+                let _ = writeln!(
+                    &mut result,
+                    "  s{state} -> s{} [label={label}];",
+                    self.next_state(state, input)
+                );
+            }
+        }
+
+        let _ = write!(&mut result, "}}");
+
+        result
     }
 
     #[cfg(debug_assertions)]
     fn validate(&self) {
-        for transition in self.transitions.iter().chain(self.init_states.iter()) {
+        for transition in self.transitions.iter() {
+            assert!(*transition < self.states);
+        }
+        for transition in self.init_states.iter() {
             assert!(*transition < self.states);
         }
     }
@@ -222,11 +280,15 @@ impl Dfa {
             reachable.insert(*init_state);
             stack.push(*init_state);
         }
+        // it's a bit easier to debug with this. For instance,
+        // combinatorial functions transition tables are just
+        // truth tables repeated twice
+        stack.reverse();
 
         while let Some(state) = stack.pop() {
             visit(state);
 
-            for label in self.alphabet() {
+            for label in self.alphabet().rev() {
                 let next_state = self.next_state(state, label);
                 if reachable.contains(next_state) {
                     continue;
@@ -291,35 +353,37 @@ impl Dfa {
     // Minimize the DFA using Hopcroft's algorithm. Does not return a canonical
     // form of the DFA. Does not prune unreachable states.
     fn hopcroft(&self) -> Dfa {
-        let mut worklist: HashSet<StateBitSet> = HashSet::new();
+        if self.accepting.cardinality() == self.states {
+            return Dfa::constant(true, self.inputs);
+        } else if self.accepting.cardinality() == 0 {
+            return Dfa::constant(false, self.inputs);
+        }
+
+        let mut worklist = BTreeSet::new();
         worklist.insert(self.accepting.clone());
         worklist.insert(!self.accepting.clone());
         let mut states_eq_classes = worklist.clone();
         // Iterate until fixpoint is reached and we computed all states in the minimal DFA.
-        loop {
-            let Some(set) = ({
-                let mut drain = worklist.drain();
-                drain.next()
-            }) else {
-                break;
-            };
+        while let Some(set) = worklist.pop_first() {
+            assert_ne!(set.cardinality(), 0);
+            assert!(set.set.iter_ones().all(|x| (x as u16) < self.states));
 
             for label in self.alphabet() {
                 let mut go_into_set = StateBitSet::new(self.states);
                 for state in self.states() {
                     let next_state = self.next_state(state, label);
                     if set.contains(next_state) {
-                        go_into_set.insert(next_state);
+                        go_into_set.insert(state);
                     }
                 }
 
-                let mut new_states_eq_classes = HashSet::new();
+                let mut new_states_eq_classes = BTreeSet::new();
                 for class in states_eq_classes.into_iter() {
                     let class_goes_into_set = &class & &go_into_set;
-                    let class_does_not_go_into_set = &class / &go_into_set;
+                    let class_does_not_go_into_set = &class - &go_into_set;
 
-                    if class_goes_into_set == go_into_set
-                        || class_does_not_go_into_set == go_into_set
+                    if class_goes_into_set.cardinality() == 0
+                        || class_does_not_go_into_set.cardinality() == 0
                     {
                         new_states_eq_classes.insert(class);
                         continue;
@@ -394,9 +458,12 @@ impl Dfa {
     ) -> Dfa {
         assert!(extra_memory_states > 0);
         assert_eq!(self.inputs, other.inputs);
+        let new_states = 2 * self.states * other.states * extra_memory_states;
 
-        let new_states = self.states * other.states * extra_memory_states;
-        let to_prod_state = |x, y, extra| (x * other.states + y) * extra_memory_states + extra;
+        let to_prod_state = |accepting, x, y, extra| {
+            (((accepting as State) * self.states + x) * other.states + y) * extra_memory_states
+                + extra
+        };
 
         // Build initial transitions
         let mut new_init_states = vec![];
@@ -404,42 +471,57 @@ impl Dfa {
         for label in self.alphabet() {
             let self_init_state = self.init_states[label];
             let other_init_state = other.init_states[label];
-            let target_state =
-                to_prod_state(self_init_state, other_init_state, initial_memory_state);
+
+            let (accepting, new_memory_state) = transition_fn(
+                self.accepting.contains(self_init_state),
+                other.accepting.contains(other_init_state),
+                initial_memory_state,
+            );
+            let target_state = to_prod_state(
+                accepting,
+                self_init_state,
+                other_init_state,
+                new_memory_state,
+            );
+
             new_init_states.push(target_state);
         }
 
         // Iterate over all states in the product DFA to build transition tables
         let mut new_transitions = vec![];
         new_transitions.reserve_exact(self.alphabet_size() * new_states as usize);
-        let mut new_accepting = StateBitSet::new(new_states);
         for self_state in 0..self.states {
             for other_state in 0..other.states {
                 for memory in 0..extra_memory_states {
-                    let mut new_transitions_for_state = vec![];
-                    let cur_prod_state = to_prod_state(self_state, other_state, memory);
-
-                    let (accepting, new_memory) = transition_fn(
-                        self.accepting.contains(self_state),
-                        other.accepting.contains(other_state),
-                        memory,
-                    );
-
-                    if accepting {
-                        new_accepting.insert(cur_prod_state);
-                    }
-
                     for label in self.alphabet() {
                         let self_next_state = self.next_state(self_state, label);
                         let other_next_state = other.next_state(other_state, label);
-                        let prod_state =
-                            to_prod_state(self_next_state, other_next_state, new_memory);
+                        let (accepting, new_memory_state) = transition_fn(
+                            self.accepting.contains(self_next_state),
+                            other.accepting.contains(other_next_state),
+                            memory,
+                        );
+                        assert!(new_memory_state < extra_memory_states);
+                        let next_prod_state = to_prod_state(
+                            accepting,
+                            self_next_state,
+                            other_next_state,
+                            new_memory_state,
+                        );
 
-                        new_transitions_for_state.push(prod_state);
+                        assert!(next_prod_state < new_states);
+                        new_transitions.push(next_prod_state);
                     }
-                    new_transitions.extend(new_transitions_for_state);
                 }
             }
+        }
+        // Repeat same transition table second time. We have to do this,
+        // since otherwise accepting/non-accepting states may alias.
+        new_transitions.extend_from_within(0..);
+
+        let mut new_accepting = StateBitSet::new(new_states);
+        for i in (new_states / 2)..new_states {
+            new_accepting.insert(i);
         }
 
         assert_eq!(
@@ -461,7 +543,7 @@ impl Dfa {
     fn input(ith: u8, out_of: u8) -> Dfa {
         let mut init_states = vec![];
         for label in 0..(1 << out_of) {
-            init_states.push(if (label & (1 << ith)) == 0 { 1 } else { 0 })
+            init_states.push(((label & (1 << ith)) != 0) as State)
         }
 
         let mut transitions = vec![];
@@ -469,7 +551,7 @@ impl Dfa {
         transitions.extend_from_slice(&init_states);
 
         let mut accepting = StateBitSet::new(2);
-        accepting.insert(0);
+        accepting.insert(1);
 
         let res = Dfa {
             inputs: out_of,
@@ -512,11 +594,27 @@ impl Dfa {
     }
 }
 
+impl std::ops::BitAnd for &Dfa {
+    type Output = Dfa;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        self.binary(rhs, 1, 0, |a, b, _| (a & b, 0)).minimize()
+    }
+}
+
 impl std::ops::BitAnd for Dfa {
     type Output = Dfa;
 
     fn bitand(self, rhs: Self) -> Self::Output {
-        self.binary(&rhs, 1, 0, |a, b, _| (a & b, 0)).minimize()
+        &self & &rhs
+    }
+}
+
+impl std::ops::BitOr for &Dfa {
+    type Output = Dfa;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        self.binary(rhs, 1, 0, |a, b, _| (a | b, 0)).minimize()
     }
 }
 
@@ -524,7 +622,15 @@ impl std::ops::BitOr for Dfa {
     type Output = Dfa;
 
     fn bitor(self, rhs: Self) -> Self::Output {
-        self.binary(&rhs, 1, 0, |a, b, _| (a | b, 0)).minimize()
+        &self | &rhs
+    }
+}
+
+impl std::ops::BitXor for &Dfa {
+    type Output = Dfa;
+
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        self.binary(rhs, 1, 0, |a, b, _| (a ^ b, 0)).minimize()
     }
 }
 
@@ -532,7 +638,7 @@ impl std::ops::BitXor for Dfa {
     type Output = Dfa;
 
     fn bitxor(self, rhs: Self) -> Self::Output {
-        self.binary(&rhs, 1, 0, |a, b, _| (a ^ b, 0)).minimize()
+        &self ^ &rhs
     }
 }
 
@@ -540,15 +646,23 @@ impl std::ops::Not for Dfa {
     type Output = Dfa;
 
     fn not(self) -> Self::Output {
-        self.unary(1, 0, |a, _| (!a, 0))
+        self.unary(1, 0, |a, _| (!a, 0)).minimize()
     }
 }
 
-impl std::ops::Add for Dfa {
+impl std::ops::Not for &Dfa {
+    type Output = Dfa;
+
+    fn not(self) -> Self::Output {
+        !self.clone()
+    }
+}
+
+impl std::ops::Add for &Dfa {
     type Output = Dfa;
 
     fn add(self, rhs: Self) -> Self::Output {
-        self.binary(&rhs, 2, 0, |a, b, carry| {
+        self.binary(rhs, 2, 0, |a, b, carry| {
             let sum = a as State + b as State + carry;
             (sum & 1 != 0, sum >> 1)
         })
@@ -556,13 +670,41 @@ impl std::ops::Add for Dfa {
     }
 }
 
+impl std::ops::Add for Dfa {
+    type Output = Dfa;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        &self + &rhs
+    }
+}
+
 impl std::ops::Neg for Dfa {
     type Output = Dfa;
 
     fn neg(self) -> Self::Output {
-        self.unary(1, 0, |a, carry| {
+        self.unary(2, 1, |a, carry| {
             let half_adder = (!a) as State + carry;
             (half_adder & 1 != 0, half_adder >> 1)
+        })
+        .minimize()
+    }
+}
+
+impl std::ops::Neg for &Dfa {
+    type Output = Dfa;
+
+    fn neg(self) -> Self::Output {
+        -self.clone()
+    }
+}
+
+impl std::ops::Sub for &Dfa {
+    type Output = Dfa;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.binary(rhs, 2, 1, |a, b, carry| {
+            let sum = a as State + (!b) as State + carry;
+            (sum & 1 != 0, sum >> 1)
         })
         .minimize()
     }
@@ -572,11 +714,7 @@ impl std::ops::Sub for Dfa {
     type Output = Dfa;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        self.binary(&rhs, 2, 1, |a, b, carry| {
-            let sum = a as State + (!b) as State + carry;
-            (sum & 1 != 0, sum >> 1)
-        })
-        .minimize()
+        &self - &rhs
     }
 }
 
@@ -590,8 +728,8 @@ mod test {
             inputs: 2,
             states: 3,
             accepting: StateBitSet::from_iter(3, [0, 2]),
-            transitions: vec![0, 1, 0, 2, 1, 2, 0, 0, 0, 2, 1, 1],
-            init_states: vec![2, 0, 0, 2],
+            transitions: vec![2, 0, 1, 1, 1, 0, 2, 2, 2, 1, 2, 0],
+            init_states: vec![0, 2, 2, 0],
         };
 
         let dfa_actual = dfa.canonical();
@@ -639,6 +777,14 @@ mod test {
     }
 
     #[test]
+    fn add_cancel() {
+        let x = || Dfa::input(0, 2);
+        let y = || Dfa::input(1, 2);
+
+        assert_eq!(x() + y() + (-y()), x());
+    }
+
+    #[test]
     fn add_assoc() {
         let left_dfa = (Dfa::input(0, 3) + Dfa::input(1, 3)) + Dfa::input(2, 3);
         let right_dfa = Dfa::input(0, 3) + (Dfa::input(1, 3) + Dfa::input(2, 3));
@@ -668,5 +814,38 @@ mod test {
         let optimized_yz = y() + !x() + Dfa::one(2);
 
         assert_eq!(unoptimized_yx, optimized_yz);
+    }
+
+    #[test]
+    fn z3_killer() {
+        // (set-logic QF_BV)
+        // (declare-fun y () (_ BitVec 64))
+        // (declare-fun x () (_ BitVec 64))
+        // (assert (let ((a!1 (bvsub (bvsub (bvsub x #x0000000000000002) y)
+        //   (bvshl (bvor x (bvnot y)) #x0000000000000001))))
+        // (let ((a!2 (bvmul (bvand (bvxor a!1 x) (bvnot y))
+        //   (bvand y (bvnot (bvxor a!1 x))))))
+        // (let ((a!3 (bvadd (bvmul (bvor (bvxor a!1 x) y) (bvand (bvxor a!1 x) y)) a!2)))
+        //   (distinct a!3 (bvmul y y))))))
+        // (check-sat)
+
+        let x = Dfa::input(0, 2);
+        let y = Dfa::input(1, 2);
+        let two = Dfa::one(2) + Dfa::one(2);
+
+        let x_or_not_y = &x | &!&y;
+
+        let a1 = (&(&x - &two) - &y) - (&x_or_not_y + &x_or_not_y);
+
+        let xor_a1_x = &a1 ^ &x;
+
+        let a2_1 = &y & &!&xor_a1_x;
+        assert_eq!(a2_1, Dfa::constant(false, 2));
+        // a2 = a2_0 * a2_1 = a2_0 * 0 = 0
+
+        let a3_0 = &xor_a1_x | &y;
+        let a3_1 = &xor_a1_x & &y;
+        assert_eq!(a3_1, y);
+        assert_eq!(a3_0, y);
     }
 }
